4e83afc296d58ae0002b9028163e68c3b9446b9e
kiennt048 <54541345+kiennt048@users.noreply.github.com>
2021-08-29 15:16:58 +0700

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -7,7 +7,7 @@ curl \
 gnupg \
 lsb-release
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg --yes
 
 echo \
 "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
